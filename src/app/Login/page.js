b5a504695635ceeeb97194a8a2b324f7c732f3a5
IDY12345 <106066822+IDY12345@users.noreply.github.com>
2023-09-20 22:07:34 +0530

    Sign up Frontend and Backend

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -15,6 +15,8 @@ const LoginOptions=
 ]
 
 function page() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   const [selectLogin, setSelectLogin] = useState("")
   const [showPass, setShowPass] = useState(false)
   const loginOptions=LoginOptions.map((option1)=>
@@ -23,15 +25,22 @@ function page() {
   </option>
   )
 
-  const handleShowPass=()=>
+  const submit=async(e)=>
   {
-    setShowPass(!showPass)
+    e,preventDefault();
+
+    await fetch('http://localhost:3000/api/login',{
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      credentials:'include',
+      body:JSON.stringify({
+        email,
+        password
+      })
+    })
+    console.log("Logged in Succesfully")
   }
 
-  const handleClick=()=>
-  {
-    <Link href={`/Login/${selectLogin}`} />
-  }
 
   return (
     <div className='bg-primary min-h-screen text-white justify-center items-center flex'>
@@ -41,18 +50,22 @@ function page() {
           <select className='bg-secondary text-[22px] ml-[30px] w-4/5 text-black' value={selectLogin} onChange={(e)=>{
             e.preventDefault
             setSelectLogin(e.target.value)
-          }} onClick={handleClick}>
+          }}>
             <option>Select</option>
             {loginOptions}
           </select>
           {
             (selectLogin==="Student") ?(
-              <form>
+              <form onSubmit={submit}>
               <h1 className='text-[22px] mt-[15px] ml-[30px]'>Student id</h1>
-              <input className='w-4/5 bg-secondary text-black border-1 border-white-600 ml-[30px] text-[22px] placeholder:text-black' type={`email`} placeholder='Student id' />
+              <input className='w-4/5 bg-secondary text-black border-1 border-white-600 ml-[30px] text-[22px] placeholder:text-black' type={`email`} onChange={(e)=>{
+              setEmail(e.target.value)
+            }} placeholder='Student id' />
               
               <h1 className='text-[22px] mt-[15px] ml-[30px]'>Password</h1>
-              <input className='w-4/5 bg-secondary text-black border-1 border-white-600 ml-[30px] text-[22px] placeholder:text-black' placeholder='Password' type={"password"} />
+              <input className='w-4/5 bg-secondary text-black border-1 border-white-600 ml-[30px] text-[22px] placeholder:text-black'             onChange={(e)=>{
+              setPassword(e.target.value)
+            }} placeholder='Password' type={"password"} />
 
               <button className='rounded-xl ml-[185px] text-black w-[150px] border-1 border-white-600 font-bold bg-secondary text-[25px] mt-[30px]'>Login</button>
               </form>
